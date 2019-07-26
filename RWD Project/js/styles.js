90d709fdb34dffdbb31bef7f089d5cd1bd3843fd
shivendrab <48115043+shivendrab@users.noreply.github.com>
2019-07-26 17:47:24 +1000

    Updated after implementation Data read through json

--- RWD Project/js/styles.js
+++ RWD Project/js/styles.js
@@ -1,83 +1,138 @@
 var mobilenamespace = {
     loadcarousel: function(t) {
-        var o = t.mobilescor;
-        $("#h4cormotog").text(o[0].h4cormotogtxt),
-        $("#h5cormotog").text(o[0].h5cormotogtxt),
-        $("#pcormotog").text(o[0].pcormotogtxt),
-        $("#btncormotog").text(o[0].btncormotogtxt),
-        $("#modmoto").text(o[0].h4cormotogtxt),
-        $("#motodesc").text(o[0].pcormotogtxt),
-        $("#h4corsony").text(o[1].h4corsonytxt),
-        $("#h5corsony").text(o[1].h5corsonytxt),
-        $("#pcorsony").text(o[1].pcorsonytxt),
-        $("#btncorsony").text(o[1].btncorsonytxt),
-        $("#modsony").text(o[1].h4corsonytxt),
-        $("#sonydesc").text(o[2].pcorwintxt),
-        $("#h4corwin").text(o[2].h4corwin),
-        $("#h5corwin").text(o[2].h5corwin),
-        $("#pcorwin").text(o[2].pcorwintxt),
-        $("#btncorwin").text(o[2].btncorwin),
-        $("#modwin").text(o[2].h4corwin),
-        $("#windesc").text(o[2].pcorwintxt)
+        var o = t.mobilecarousel;
+		
+		$("#imgcarousel1").attr("alt", o[0].id),
+        $("#h4carousel1txt").text(o[0].h4carousel1txt),
+        $("#strongcarousel1txt").text(o[0].strongcarousel1txt),
+        $("#pcarousel1txt").text(o[0].pcarousel1txt),
+        $("#btncarousel1txt").text(o[0].btncarousel1txt),
+		
+		$("#imgcarousel2").attr("alt", o[1].id),
+        $("#h4carousel2txt").text(o[1].h4carousel2txt),
+        $("#strongcarousel2txt").text(o[1].strongcarousel2txt),
+        $("#pcarousel2txt").text(o[1].pcarousel2txt),
+        $("#btncarousel2txt").text(o[1].btncarousel2txt),
+		
+		$("#imgcarousel3").attr("alt", o[2].id),
+        $("#h4carousel3txt").text(o[2].h4carousel3txt),
+        $("#strongcarousel3txt").text(o[2].strongcarousel3txt),
+        $("#pcarousel3txt").text(o[2].pcarousel3txt),
+        $("#btncarousel3txt").text(o[2].btncarousel3txt)
     },
     loaddata: function(t) {
         this.loadcarousel(t),
-        this.loadimage(t),
+        this.loadimage(t),		
+		
         $("#home").text(t.navbar[0]),
         $("#Mobiles").text(t.navbar[1]),
         $("#hotdeal").text(t.navbar[2]),
         $("#about").text(t.navbar[3]),
         $("#contact").text(t.navbar[4]),
-        $("#pseller").text(t.info[1].description),
-        $("#spannew").text(t.info[0].label0),
-        $("#spanarrv").text(t.info[0].label01),
-        $("#parrivals").text(t.info[0].description),
-        $("#salebest").text(t.info[1].label0),
-        $("#saledark").text(t.info[1].label01),
-        $("#pseller").text(t.info[1].description),
-        $("#txtloadall").text(t.info[4].label0),
-        $("#subscribeNow").text(t.payments[0].subscription),
-        $("#Subscribe").html(t.payments[1].btnsubscribe),
-        $("#bnwltrtext").html(t.payments[1].bnwltrtext),
-        $("#paymenticons").append(t.payments[0].description),
-        $("#about1").append(t.footer[0].aboutfooter),
+		
+		$("#lnkNewArrival1").text(t.newarrivals[0].label0),
+		$("#lnkNewArrival2").text(t.newarrivals[1].label1),
+		$("#lnkNewArrival3").text(t.newarrivals[2].label2),
+		$("#lnkNewArrival4").text(t.newarrivals[3].label3),
+		
+		$("#imgNewArrival1").attr("alt", t.newarrivals[0].label0),
+		$("#imgNewArrival2").attr("alt", t.newarrivals[1].label1),
+		$("#imgNewArrival3").attr("alt", t.newarrivals[2].label2),
+		$("#imgNewArrival4").attr("alt", t.newarrivals[3].label3),
+		
+		$("#imgNewArrivalcart1").attr("alt", t.newarrivals[0].description),
+		$("#imgNewArrivalcart1").attr("alt", t.newarrivals[1].description),
+		$("#imgNewArrivalcart1").attr("alt", t.newarrivals[2].description),
+		$("#imgNewArrivalcart1").attr("alt", t.newarrivals[3].description),
+		
+		$("#imgNewArrivalWish1").attr("alt", t.newarrivals[0].description),
+		$("#imgNewArrivalWish2").attr("alt", t.newarrivals[1].description),
+		$("#imgNewArrivalWish3").attr("alt", t.newarrivals[2].description),
+		$("#imgNewArrivalWish4").attr("alt", t.newarrivals[3].description),
+		        
+        $("#newArrivalMain1").text(t.information[0].Mainlabel01),
+        $("#newArrivalMain2").text(t.information[0].Mainlabel02),
+        $("#newArrivalDesc").text(t.information[0].description),
+		
+		$("#bestSalesMain1").text(t.information[1].Mainlabel01),
+        $("#bestSalesMain2").text(t.information[1].Mainlabel02),
+        $("#bestSalesDesc").text(t.information[1].description),
+		
+		$("#h3LoadAllbtntxt").text(t.information[4].Mainlabel01),
+		
+		$("#imgSalesProduct1").attr("alt", t.sales[0].label0),
+		$("#imgSalesProduct2").attr("alt", t.sales[1].label1),
+		$("#imgSalesProduct3").attr("alt", t.sales[2].label2),
+		
+		$("#lnkSalesProduct1").text(t.sales[0].label0),
+		$("#lnkSalesProduct2").text(t.sales[1].label1),
+		$("#lnkSalesProduct3").text(t.sales[2].label2),
+		
+		$("#userReviewsMain1").text(t.information[2].Mainlabel01),
+        $("#userReviewsMain2").text(t.information[2].Mainlabel02),
+        $("#userReviewsDesc").text(t.information[2].description),		
+		
+		$("#imgReviewsProduct1").attr("alt", t.reviews[0].id),
+		$("#imgReviewsProduct2").attr("alt", t.reviews[1].id),
+		$("#imgReviewsProduct3").attr("alt", t.reviews[2].id),
+		
+		$("#txtReviewsProduct1").text(t.reviews[0].description),
+		$("#txtReviewsProduct2").text(t.reviews[1].description),
+		$("#txtReviewsProduct3").text(t.reviews[2].description),
+		
+		$("#txtNewsLetter").text(t.information[3].Mainlabel01),
+        $("#txtTypeEmail").text(t.information[3].Mainlabel02),
+        $("#txtNewsLetterDesc").text(t.information[3].description),
+		$("#btnSubscribe").text(t.information[3].buttontext),
+		
+		$("#imgEmail").attr("alt", t.information[4].Mainlabel02),
+		
+		$("#about1").append(t.footer[0].aboutfooter),
         $("#Sub1").append(t.footer[0].subfooter),
-        $("#Sub2").append(t.footer[0].subfooter)
+		$("#Sub2").append(t.footer[0].subfooter),
+		
+		$("#imgpayment").attr("alt", t.payments[2].description),
+		
+        $("#paymenticons").append(t.payments[0].description)      
+        
     },
     loadimage: function(t) {
-        var o = t.imagesinfo[0];
+        var o = t.imageLoad[0];
+		
+		$("#imgcarousel1").attr("src", o.imgcarousel1),
+		$("#imgcarousel2").attr("src", o.imgcarousel2),
+		$("#imgcarousel3").attr("src", o.imgcarousel3),
+		
+		$("#imgNewArrival1").attr("src", o.imgNewArrival1),
+		$("#imgNewArrival2").attr("src", o.imgNewArrival2),
+		$("#imgNewArrival3").attr("src", o.imgNewArrival3),
+		$("#imgNewArrival4").attr("src", o.imgNewArrival4),
+		
+		$("#imgNewArrivalcart1").attr("src", o.imgNewArrivalcart1),
+		$("#imgNewArrivalcart2").attr("src", o.imgNewArrivalcart2),
+		$("#imgNewArrivalcart3").attr("src", o.imgNewArrivalcart3),
+		$("#imgNewArrivalcart4").attr("src", o.imgNewArrivalcart4),
+		
+		$("#imgNewArrivalWish1").attr("src", o.imgNewArrivalWish1),
+		$("#imgNewArrivalWish2").attr("src", o.imgNewArrivalWish2),
+		$("#imgNewArrivalWish3").attr("src", o.imgNewArrivalWish3),
+		$("#imgNewArrivalWish4").attr("src", o.imgNewArrivalWish4),
+		
+		$("#imgSalesProduct1").attr("src", o.imgSalesProduct1),
+		$("#imgSalesProduct2").attr("src", o.imgSalesProduct2),
+		$("#imgSalesProduct3").attr("src", o.imgSalesProduct3),
+		
+		$("#imgReviewsProduct1").attr("src", o.imgReviewsProduct1),
+		$("#imgReviewsProduct2").attr("src", o.imgReviewsProduct2),
+		$("#imgReviewsProduct3").attr("src", o.imgReviewsProduct3),
+		
+		$("#imgEmail").attr("src", o.imgemail),
+		$("#imgpayment").attr("src", o.imgpaymenticon),
+		
         $("#headersrc").attr("src", o.imgsearch24),
         $("#headerusr").attr("src", o.imguser24),
         $("#headercart").attr("src", o.imgcart32),
-        $("#imglogo").attr("src", o.imglogo),
-        $("#motocr").attr("src", o.imgcarosuel),
-        $("#sonycr").attr("src", o.imgsony),
-        $("#wincr").attr("src", o.imgwindowcell),
-        $("#motomob1").attr("src", o.imgmobile2),
-        $("#xprcart").attr("src", o.imgmobile1),
-        $("#mobile2").attr("src", o.imgmobile2),
-        $("#motocart").attr("src", o.imgcart),
-        $("#motoxcart").attr("src", o.imgcart),
-        $("#cartId").attr("src", o.imgcart),
-        $("#whishlist1").attr("src", o.imgwhishlist),
-        $("#whishlist2").attr("src", o.imgwhishlist),
-        $("#whishlist3").attr("src", o.imgwhishlist),
-        $("#mobile12").attr("src", o.imgmobile2),
-        $("#mobile1xp").attr("src", o.imgmobile1),
-        $("#mobilemoto2").attr("src", o.imgmobile2),
-        $("#mobilecart").attr("src", o.imgcart),
-        $("#motocart").attr("src", o.imgcart),
-        $("#mobile1cart").attr("src", o.imgcart),
-        $("#mobile2cart").attr("src", o.imgcart),
-        $("#mobileemail").attr("src", o.imgemail),
-        $("#homembl").attr("src", o.imghome24),
-        $("#mobilenews").attr("src", o.imgmobile24),
-        $("#mobiledeals").attr("src", o.imggas24),
-        $("#mobilecontact").attr("src", o.imgcontact),
-        $("#mobileabout").attr("src", o.imgcons24),
-        $("#imgpayment").attr("src", o.imgpaymenticon),
-        $("#modimgsony").attr("src", o.imgsony),
-        $("#modimgmoto").attr("src", o.imgcarosuel),
-        $("#modimgwin").attr("src", o.imgwindowcell)
+        $("#imglogo").attr("src", o.imglogo) 
+		$("#whishlist1").attr("src", o.whishlist1)		
     }
 };
